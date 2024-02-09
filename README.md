26515b8546be66204a6ee48742cb9a83eb8503ea
Maximilian <hallo@maximilianberndt.com>
2024-02-09 10:24:10 +0100

    Add tweakpane fps plugin

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ogl-starter
 
-Starter template for [ogl](https://github.com/oframe/ogl) with [vite](https://vitejs.dev/), [vite-plugin-glsl](https://www.npmjs.com/package/vite-plugin-glsl), [tweakpane](https://cocopon.github.io/tweakpane/) and [stats-gl](https://github.com/RenaudRohlinger/stats-gl).
+Starter template for [ogl](https://github.com/oframe/ogl) with [vite](https://vitejs.dev/), [vite-plugin-glsl](https://www.npmjs.com/package/vite-plugin-glsl) and [tweakpane](https://cocopon.github.io/tweakpane/).
 
 ## Installation
 
--- src/js/debug.ts
+++ src/js/debug.ts
@@ -1,15 +1,17 @@
+import * as EssentialsPlugin from '@tweakpane/plugin-essentials'
 import { Pane } from 'tweakpane'
 import { onFrame } from './app'
 
-const options = {
-  fps: 0,
-}
-
 export const gui = new Pane()
+gui.registerPlugin(EssentialsPlugin)
 
 const folder = gui.addFolder({ title: 'Debug' })
-folder.addBinding(options, 'fps', { readonly: true })
+const fpsGraph = folder.addBlade({ view: 'fpsgraph', rows: 2 })
 
-onFrame((t, deltaTime) => {
-  options.fps = 1000 / deltaTime
+onFrame(() => {
+  fpsGraph.begin()
+}, -Infinity)
+
+onFrame(() => {
+  fpsGraph.end()
 }, Infinity)
