c448ee754000a669160724ea2b45e3d1523946d0
Maximilian <hallo@maximilianberndt.com>
2024-03-29 10:54:31 +0100

    Cleanup

--- src/js/components/camera/index.ts
+++ src/js/components/camera/index.ts
@@ -1,9 +1,15 @@
-import { Camera } from 'ogl'
-import { onResize } from '../../app'
-import { gl } from '../../renderer'
+import { Camera, Orbit } from 'ogl'
+import { onFrame, onResize } from '../../app'
+import { canvas, gl } from '../../renderer'
 
 const camera = new Camera(gl, { fov: 75 })
 camera.position.z = 5
+
+const controls = new Orbit(camera, {
+  element: canvas!.parentNode as HTMLDivElement,
+})
+
+onFrame(controls.update)
 
 onResize(({ width = 1, height = 1 }) => {
   camera.perspective({
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,25 +1,13 @@
-import { Orbit } from 'ogl'
 import '../css/style.css'
 import { onFrame, onResize } from './app'
 import camera from './components/camera'
 import { mesh } from './components/shape'
-import {
-  canvas,
-  gl,
-  post,
-  renderer,
-  resolution,
-  scene,
-} from './renderer'
+import { gl, post, renderer, resolution, scene } from './renderer'
 
-const controls = new Orbit(camera, {
-  element: canvas!.parentNode as HTMLDivElement,
-})
 mesh.setParent(scene)
 
 gl.clearColor(1, 1, 1, 1)
 
-onFrame(controls.update)
 onFrame(() => {
   post.render({ scene, camera })
 }, Infinity - 1)
